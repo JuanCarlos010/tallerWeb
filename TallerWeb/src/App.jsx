10f81b78140557917ed87c3bf372c24ab5627c25
EduardoSig2210 <0001252@uca.edu.sv>
2025-10-14 21:04:10 -0600

    Feat: Creacion del la pantalla en App.jsx

--- TallerWeb/src/App.jsx
+++ TallerWeb/src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [idItem, setIdItem] = useState("");
+  const [rresultado, setResultado] = useState(null);
+  const [error, setError] = useState(null);
+
+  const ConsultarAPI = async () => {
+    try {
+      setErr(null);
+      setResultado(null);
+
+      const respuesta = await fetch("http://localhost:3000/dataInfo/idItem") // cambiar idItem por el id del elemento
+      if(!respuesta.ok){
+        throw new Error("Err En la consulta con el Id: idItem")
+      }
+
+      const data = await respuesta.json();
+      setResultado(data);
+    } catch(err){
+      setError(err.mensaje)
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Consulta de Items</h1>
+      <input
+        type="number"
+        placeholder="Ingrese ID del item"
+        value={idItem}
+        onChange={(e) => setIdItem(e.target.value)}
+      />
+      <button onClick={consultarAPI} style={{ marginLeft: "10px" }}>
+        Consultar
+      </button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {resultado && resultado.status && (
+        <div style={{ marginTop: "20px" }}>
+          <h3>Resultado:</h3>
+          <ul>
+            <li><strong>ID:</strong> {resultado.item.id}</li>
+            <li><strong>Nombre:</strong> {resultado.item.nombre}</li>
+            <li><strong>Precio:</strong> ${resultado.item.precio}</li>
+            <li><strong>Fecha Respuesta:</strong> {resultado.dateTime}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
